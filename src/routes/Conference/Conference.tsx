6dbdfc33a493a1979d6df41b9318b34b27113ada
Eric Cha <z5324960@ad.unsw.edu.au>
2023-07-03 14:43:05 +1000

    add

--- src/routes/Conference/Conference.tsx
+++ src/routes/Conference/Conference.tsx
@@ -326,8 +326,8 @@ export const Conference = () => {
                   pl={isDesktop && isPresentationActive ? 'm' : isTablet && isLandscape && isPresentationActive && 'xs'}
                 >
                   <ParticipantsGrid
-                    localText={intl.formatMessage({ id: 'you' })}
-
+                    //localText={intl.formatMessage({ id: 'you' })}
+                    localText={intl.formatMessage({ id: 'str' })}
                     testID="ParticipantsGrid"
                     additionalContainerStyle={
                       isOneParticipant && !isDesktop
